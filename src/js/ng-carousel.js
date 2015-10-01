54a3a9970e37511f28f41b652581b9a42b077a03
Nathaniel Huff <ngh717@gmail.com>
2015-10-01 10:48:49 -0400

    Initialize repo with bootstrapped files.

---
+++ src/js/ng-carousel.js
@@ -0,0 +1,33 @@
+/**
+ * @license ng-carousel.js v0.0.1
+ * (c) 2015 Nathaniel Smith http://nathanielhuff.com/
+ * License: MIT
+ */
+(function (window, angular, undefined) {
+
+  'use strict';
+
+  angular
+    .module('app', []);
+
+  // MainCtrl.js
+  function MainCtrl () {
+
+  }
+
+  angular
+    .module('app')
+    .controller('MainCtrl', MainCtrl);
+
+  // SomeService.js
+  function SomeService () {
+
+  }
+
+  angular
+    .module('app')
+    .service('SomeService', SomeService);
+
+  // ...
+
+})(window, window.angular);
